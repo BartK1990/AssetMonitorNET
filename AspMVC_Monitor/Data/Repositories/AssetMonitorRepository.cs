acf4e6ed54a88479161ccfcb4d9bf3db34e28fac
Bartosz Kuriata <kuriata.bartosz@gmail.com>
2021-12-11 20:53:11 +0100

    Repository pattern

---
+++ AspMVC_Monitor/Data/Repositories/AssetMonitorRepository.cs
@@ -0,0 +1,29 @@
+﻿using AssetMonitorDataAccess.DataAccess;
+using AssetMonitorDataAccess.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspMVC_Monitor.Data.Repositories
+{
+    public class AssetMonitorRepository : IAssetMonitorRepository
+    {
+        private readonly AssetMonitorContext _context;
+
+        public AssetMonitorRepository(AssetMonitorContext context)
+        {
+            _context = context;
+        }
+
+        public IEnumerable<Asset> GetAllAssets()
+        {
+            return _context.Assets.ToList();
+        }
+
+        public bool SaveAll()
+        {
+            return _context.SaveChanges() > 0;
+        }
+    }
+}
---
+++ AspMVC_Monitor/Data/Repositories/IAssetMonitorRepository.cs
@@ -0,0 +1,11 @@
+﻿using AssetMonitorDataAccess.Models;
+using System.Collections.Generic;
+
+namespace AspMVC_Monitor.Data.Repositories
+{
+    public interface IAssetMonitorRepository
+    {
+        IEnumerable<Asset> GetAllAssets();
+        bool SaveAll();
+    }
+}
