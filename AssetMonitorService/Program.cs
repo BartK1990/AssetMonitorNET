7647e448ae252edc621a1c84af4ac8df0ebcddc6
Bartosz Kuriata <kuriata.bartosz@gmail.com>
2021-12-30 15:59:10 +0100

    Added Serilog as logging

--- AssetMonitorService/Program.cs
+++ AssetMonitorService/Program.cs
@@ -1,10 +1,8 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
+using Serilog;
 using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace AssetMonitorService
 {
@@ -12,7 +10,29 @@ namespace AssetMonitorService
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var dir = Directory.GetCurrentDirectory();
+            Log.Logger = new LoggerConfiguration()
+                .Enrich.FromLogContext()
+                .WriteTo.Console()
+                .WriteTo.File($@"{dir}\Logs\Log_{DateTime.Now:yyyyMMdd-HHmm}.txt",
+                    rollOnFileSizeLimit: true,
+                    fileSizeLimitBytes: 100000000,
+                    retainedFileCountLimit: 4)
+                .CreateLogger();
+
+            try
+            {
+                Log.Information("Starting up");
+                CreateHostBuilder(args).Build().Run();
+            }
+            catch (Exception ex)
+            {
+                Log.Fatal(ex, "Application start-up failed");
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
         }
 
         // Additional configuration is required to successfully run gRPC on macOS.
@@ -20,6 +40,7 @@ namespace AssetMonitorService
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .UseWindowsService()
+                .UseSerilog()
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
