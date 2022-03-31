33ab2d48ba758ef43a8a576689795a88bd72375e
Bartosz Kuriata <kuriata.bartosz@gmail.com>
2022-03-31 11:47:02 +0200

    Description

---
+++ AssetMonitorDataAccess/Migrations/20220331094601_TagSharedDesc.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AssetMonitorDataAccess.Migrations
+{
+    public partial class TagSharedDesc : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "TagShared",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Enable",
+                value: true);
+
+            migrationBuilder.UpdateData(
+                table: "TagShared",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "Enable",
+                value: true);
+
+            migrationBuilder.UpdateData(
+                table: "TagShared",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "Enable",
+                value: true);
+
+            migrationBuilder.UpdateData(
+                table: "TagShared",
+                keyColumn: "Id",
+                keyValue: 4,
+                columns: new[] { "ColumnName", "Enable" },
+                values: new object[] { "CPU Usage [%]", true });
+
+            migrationBuilder.UpdateData(
+                table: "TagShared",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "Enable",
+                value: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "TagShared",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Enable",
+                value: false);
+
+            migrationBuilder.UpdateData(
+                table: "TagShared",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "Enable",
+                value: false);
+
+            migrationBuilder.UpdateData(
+                table: "TagShared",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "Enable",
+                value: false);
+
+            migrationBuilder.UpdateData(
+                table: "TagShared",
+                keyColumn: "Id",
+                keyValue: 4,
+                columns: new[] { "ColumnName", "Enable" },
+                values: new object[] { "CPU [%]", false });
+
+            migrationBuilder.UpdateData(
+                table: "TagShared",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "Enable",
+                value: false);
+        }
+    }
+}
